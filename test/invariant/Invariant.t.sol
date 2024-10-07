8d9a437f13e569b5f1b5c107fbdeb5df3d896e36
Sim-Security <simonar.adam@gmail.com>
2024-10-07 14:40:50 -0400

    modules

---
+++ test/invariant/Invariant.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/Invariant.sol";
+
+contract Invariant is StdInvariant, Test {
+
+}
